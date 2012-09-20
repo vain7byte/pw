f949f305757bbf596a421ceaf78424fed899b6ec
Stefan Tatschner <stefan.tatschner@gmail.com>
2012-09-20 23:14:04 +0200

    inital commit

---
+++ config/database.sql
@@ -0,0 +1,16 @@
+-- ********************************************************
+-- *                                                      *
+-- * IMPORTANT NOTE                                       *
+-- *                                                      *
+-- * Do not import this file manually but use the Contao  *
+-- * install tool to create and maintain database tables! *
+-- *                                                      *
+-- ********************************************************
+
+-- 
+-- Table `tl_pauschalen`
+-- 
+
+CREATE TABLE `tl_content` (
+    `pauschale` mediumtext NULL,
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ dca/tl_content.php
@@ -0,0 +1,34 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Stefan Tatschner 2012 
+ * @author     Stefan Tatschner 
+ * @package    pw 
+ * @license    GNU/LPGL 
+ * @filesource
+ */
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['pauschale'] = '{type_legend},type,headline;{text_legend},text;{image_legend},addImage;{protected_legend:hide},protected;{expert_legend:hide},guests,invisible,cssID,space';
+
+
+?>
